7d89c5b45ebdc2b1990f63dd2a3ca999f0be7cbd
S50B32 <pwielgosik@gmail.com>
2020-10-19 02:24:05 +0200

    Update Logo.js
    alt text added

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,25 +1,33 @@
-import React from 'react'
-import styled from 'styled-components'
-import jaglanaLogo from '../../images/jaglana-logo-white.png'
-import scrollTo from 'gatsby-plugin-smoothscroll'
-
+import React from "react"
+import styled from "styled-components"
+import jaglanaLogo from "../../images/jaglana-logo-white.png"
+import scrollTo from "gatsby-plugin-smoothscroll"
 
 const StyledLogo = styled.div`
-    height: 32rem;
-    width: 32rem;
-    border-radius: 50%;
-    background: radial-gradient(circle, rgba(0,0,0,.5) 0%, rgba(0,0,0,0) 70%, rgba(0,0,0,0) 100%);
+  height: 32rem;
+  width: 32rem;
+  border-radius: 50%;
+  background: radial-gradient(
+    circle,
+    rgba(0, 0, 0, 0.5) 0%,
+    rgba(0, 0, 0, 0) 70%,
+    rgba(0, 0, 0, 0) 100%
+  );
 
-    img{
-        height: 100%;
-        opacity: .8;
-    }
+  img {
+    height: 100%;
+    opacity: 0.8;
+  }
 `
 
 const Logo = () => (
-    <StyledLogo>
-        <img src={jaglanaLogo} onClick={() => scrollTo(`#about-us`)} />
-    </StyledLogo>
+  <StyledLogo>
+    <img
+      alt="logo restauracji Jaglana"
+      src={jaglanaLogo}
+      onClick={() => scrollTo(`#about-us`)}
+    />
+  </StyledLogo>
 )
 
-export default Logo;
+export default Logo

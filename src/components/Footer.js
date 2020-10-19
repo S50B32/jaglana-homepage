a3d9dfebba986195a25c8ce542ae1b9d465ebb0e
S50B32 <pwielgosik@gmail.com>
2020-10-19 02:12:41 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,122 +25,111 @@ const StyledWrapper = styled.footer`
   }
 `
 const FooterData = styled.div`
-    height: 87%;
-    width: 100%;
-    background-color: rgba(239, 167, 57, 1);
+  height: 87%;
+  width: 100%;
+  background-color: rgba(239, 167, 57, 1);
 
+  display: flex;
+  justify-content: left;
+  align-items: center;
 
+  position: relative;
+
+  .footerSection {
     display: flex;
+    box-sizing: border-box;
+
+    position: relative;
+    height: 100%;
+    width: 10%;
+
+    padding: 1.5rem 0 0;
+    margin: 0;
+
+    justify-content: center;
+
+    ul {
+      padding: 0;
+      margin: 0;
+      list-style: none;
+    }
+    h4 {
+      padding: 0;
+      margin: 0;
+    }
+  }
+
+  .footerSection__logo {
+    padding: 0;
+    align-items: center;
+    margin-left: 10.5%;
+  }
+  .footerSection__contact {
+    width: 7%;
+  }
+  .footerSection__about {
+    width: 7%;
+  }
+  .footerSection__info {
+    width: 7%;
+  }
+  .footerSection__media {
+    display: flex;
+    flex-direction: column;
     justify-content: left;
     align-items: center;
 
-    position: relative;
+    margin-left: 22.5%;
 
-    .footerSection{
-        display: flex;
-        box-sizing: border-box;
+    span {
+      display: flex;
+      flex-direction: column;
+      justify-content: left;
+      align-items: center;
 
-        position: relative;
-        height: 100%;
-        width: 10%;
-
-        padding: 1.5rem 0 0;
-        margin: 0;
-
-        justify-content: center;
-
-        ul{
-            padding: 0;
-            margin: 0;
-            list-style: none;
-        }
-        h4{
-            padding: 0;
-            margin: 0;
-        }
+      a {
+        width: 15%;
+        opacity: 0.6;
+        margin: 0.8rem;
+      }
     }
+  }
+  .footerSection__deliveries {
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+    align-items: center;
 
-    .footerSection__logo{
-        padding: 0;
-        align-items: center;
-        margin-left: 10.5%;
+    span {
+      display: flex;
+      flex-direction: column;
+      justify-content: left;
+      align-items: center;
 
+      a {
+        width: 50%;
+        opacity: 0.6;
+        margin: 0.7rem;
+      }
     }
-    .footerSection__contact{
-        width: 7%;
+  }
 
+  link {
+    font-family: "Cairo";
+    background: none;
+    font-size: 4rem;
+    opacity: 0.85;
+    border: none;
+    text-decoration: none;
+    color: white;
+    text-transform: lowercase;
+    margin: 0;
+    padding: 0;
+
+    :hover {
+      cursor: pointer;
     }
-    .footerSection__about{
-        width: 7%;
-
-    }
-    .footerSection__info{
-        width: 7%;
-
-    }
-    .footerSection__media{
-        display: flex;
-        flex-direction: column;
-        justify-content: left;
-        align-items: center;
-        
-        margin-left: 22.5%;
-
-        span{
-            
-            display: flex;
-            flex-direction: column;
-            justify-content: left;
-            align-items: center;
-
-            a{
-                width: 15%;
-                opacity: .6;
-                margin: .8rem;
-            }
-    
-        }
-        
-    }
-    .footerSection__deliveries{
-        display: flex;
-        flex-direction: column;
-        justify-content: left;
-        align-items: center;
-
-        
-
-        span{
-            
-            display: flex;
-            flex-direction: column;
-            justify-content: left;
-            align-items: center;
-
-            a{
-                width: 50%;
-                opacity: .6;
-                margin: .7rem;
-            
-        }
-    }
-
-    Link{
-        font-family: 'Cairo';
-        background: none;
-        font-size: 4rem;
-        opacity: .85;
-        border: none;
-        text-decoration: none;
-        color: white;
-        text-transform: lowercase;
-        margin: 0;
-        padding: 0;
-
-        :hover{
-            cursor: pointer;
-    }
-    }
+  }
 `
 
 const CreditsBar = styled.div`
